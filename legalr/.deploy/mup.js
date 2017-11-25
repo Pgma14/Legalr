1c9aa2103b366d6834323367b36dcec7941fa741
George Mgaloblishvili <gm2665@columbia.edu>
2017-11-25 15:09:51 -0500

    Set up deployment system
    Use "mup.cmd deploy" to deploy app

---
+++ legalr/.deploy/mup.js
@@ -0,0 +1,58 @@
+module.exports = {
+  servers: {
+    one: {
+      // TODO: set host address, username, and authentication method
+      host: '138.197.47.131',
+      username: 'root',
+      pem: 'C:/Users/G/new_private_key.ppk'
+      // password: 'server-password'
+      // or neither for authenticate from ssh-agent
+    }
+  },
+
+  app: {
+    // TODO: change app name and path
+    name: 'legalr',
+    path: 'C:/Users/G/Documents/GitHub/Legalr_App/legalr',
+
+    servers: {
+      one: {},
+    },
+
+    buildOptions: {
+      serverOnly: true,
+    },
+
+    env: {
+      // TODO: Change to your app's url
+      // If you are using ssl, it needs to start with https://
+      ROOT_URL: 'http://legalr.co',
+      MONGO_URL: 'mongodb://localhost/meteor',
+    },
+
+    // ssl: { // (optional)
+    //   // Enables let's encrypt (optional)
+    //   autogenerate: {
+    //     email: 'email.address@domain.com',
+    //     // comma separated list of domains
+    //     domains: 'website.com,www.website.com'
+    //   }
+    // },
+
+    docker: {
+      // change to 'abernix/meteord:base' if your app is using Meteor 1.4 - 1.5
+      image: 'abernix/meteord:node-8.4.0-base',
+    },
+
+    // Show progress bar while uploading bundle to server
+    // You might need to disable it on CI servers
+    enableUploadProgressBar: true
+  },
+
+  mongo: {
+    version: '3.4.1',
+    servers: {
+      one: {}
+    }
+  }
+};
