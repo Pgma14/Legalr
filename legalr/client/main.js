53d8e590a151ba039ae7bb4afd767c8e90fda9cd
George Mgaloblishvili <gm2665@columbia.edu>
2018-01-21 13:55:01 -0500

    Cut out redundant code from main.js
    Switch from Blaze + JQuery to ReactJS to be done in next update

--- legalr/client/main.js
+++ legalr/client/main.js
@@ -3,281 +3,40 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './index.html';
 
-
 import Popper from 'popper.js';
 window.Popper = Popper;
 
 
-
-
-
-
-
-
+var mapSections = ['click #us_map path', 'click #us_map polygon',
+'click #canada_map path', 'click #canada_map polygon', 'click #eu_map path',
+'click #eu_map polygon', '#france_map path', '#france_map polygon']
 
 Template.body.events({
   // Choose State
-  "click #us_map path": function (event) {
-    Session.set("filterByState", event.target.id);
-
+  "click path": function (event) {
     // Scroll to Results
     // target element id
     var id = $(".data_table_header");
-
-    // target element
-    var $id = $(id);
-    if ($id.length === 0) {
-        return;
-    }
-
     // top position relative to the document
     var pos = $(id).offset().top;
-
     // animated top scrolling
     $('body, html').animate({scrollTop: pos}, 1000);
-
     // Set state select
     document.getElementById("choose-state").value = event.target.id;
-
-    // Deactivate active state
-    // There should only be one active at a time
-    var elms = document.getElementsByClassName("active-state");
-    if(elms.length > 0)
-      elms[0].setAttribute("class", "land");
-
-    // Activate new state
-    event.target.setAttribute("class", "land active-state");
   },
 
-
-
-  "click #us_map polygon": function (event) {
+  "click polygon": function (event) {
     Session.set("filterByState", event.target.id);
-
   // Scroll to Results
   // target element id
   var id = $(".data_table_header");
-
-  // target element
-  var $id = $(id);
-  if ($id.length === 0) {
-      return;
-  }
-
   // top position relative to the document
   var pos = $(id).offset().top;
-
   // animated top scrolling
   $('body, html').animate({scrollTop: pos}, 1000);
-
   // Set state select
   document.getElementById("choose-state").value = event.target.id;
-
-  // Deactivate active state
-  // There should only be one active at a time
-  var elms = document.getElementsByClassName("active-state");
-  if(elms.length > 0)
-    elms[0].setAttribute("class", "land");
-
-  // Activate new state
-  event.target.setAttribute("class", "land active-state");
   },
 
-
-  "click #canada_map path": function (event) {
-    Session.set("filterByState", event.target.id);
-
-  // Scroll to Results
-  // target element id
-  var id = $("#search_heading");
-
-  // target element
-  var $id = $(id);
-  if ($id.length === 0) {
-      return;
-  }
-
-  // top position relative to the document
-  var pos = $(id).offset().top;
-
-  // animated top scrolling
-  $('body, html').animate({scrollTop: pos}, 1000);
-
-  // Set state select
-  document.getElementById("choose-state").value = event.target.id;
-
-  // Deactivate active state
-  // There should only be one active at a time
-  var elms = document.getElementsByClassName("active-state");
-  if(elms.length > 0)
-    elms[0].setAttribute("class", "land");
-
-  // Activate new state
-  event.target.setAttribute("class", "land active-state");
-  },
-
-  "click #canada_map polygon": function (event) {
-    Session.set("filterByState", event.target.id);
-
-  // Scroll to Results
-  // target element id
-  var id = $("#search_heading");
-
-  // target element
-  var $id = $(id);
-  if ($id.length === 0) {
-      return;
-  }
-
-  // top position relative to the document
-  var pos = $(id).offset().top;
-
-  // animated top scrolling
-  $('body, html').animate({scrollTop: pos}, 1000);
-
-  // Set state select
-  document.getElementById("choose-state").value = event.target.id;
-
-  // Deactivate active state
-  // There should only be one active at a time
-  var elms = document.getElementsByClassName("active-state");
-  if(elms.length > 0)
-    elms[0].setAttribute("class", "land");
-
-  // Activate new state
-  event.target.setAttribute("class", "land active-state");
-  },
-
-
-  "click #eu_map path": function (event) {
-    Session.set("filterByState", event.target.id);
-
-  // Scroll to Results
-  // target element id
-  var id = $("#search_heading");
-
-  // target element
-  var $id = $(id);
-  if ($id.length === 0) {
-      return;
-  }
-
-  // top position relative to the document
-  var pos = $(id).offset().top;
-
-  // animated top scrolling
-  $('body, html').animate({scrollTop: pos}, 1000);
-
-  // Set state select
-  document.getElementById("choose-state").value = event.target.id;
-
-  // Deactivate active state
-  // There should only be one active at a time
-  var elms = document.getElementsByClassName("active-state");
-  if(elms.length > 0)
-    elms[0].setAttribute("class", "land");
-
-  // Activate new state
-  event.target.setAttribute("class", "land active-state");
-  },
-
-  "click #eu_map polygon": function (event) {
-    Session.set("filterByState", event.target.id);
-
-  // Scroll to Results
-  // target element id
-  var id = $("#search_heading");
-
-  // target element
-  var $id = $(id);
-  if ($id.length === 0) {
-      return;
-  }
-
-  // top position relative to the document
-  var pos = $(id).offset().top;
-
-  // animated top scrolling
-  $('body, html').animate({scrollTop: pos}, 1000);
-
-  // Set state select
-  document.getElementById("choose-state").value = event.target.id;
-
-  // Deactivate active state
-  // There should only be one active at a time
-  var elms = document.getElementsByClassName("active-state");
-  if(elms.length > 0)
-    elms[0].setAttribute("class", "land");
-
-  // Activate new state
-  event.target.setAttribute("class", "land active-state");
-  },
-
-  "click #france_map path": function (event) {
-    Session.set("filterByState", event.target.id);
-
-  // Scroll to Results
-
-
-
-  // target element id
-  var id = $("#search_heading");
-
-  // target element
-  var $id = $(id);
-  if ($id.length === 0) {
-      return;
-  }
-
-  // top position relative to the document
-  var pos = $(id).offset().top;
-
-  // animated top scrolling
-  $('body, html').animate({scrollTop: pos}, 1000);
-
-  // Set state select
-  document.getElementById("choose-state").value = event.target.id;
-
-  // Deactivate active state
-  // There should only be one active at a time
-  var elms = document.getElementsByClassName("active-state");
-  if(elms.length > 0)
-    elms[0].setAttribute("class", "land");
-
-  // Activate new state
-  event.target.setAttribute("class", "land active-state");
-  },
-
-  "click #france_map polygon": function (event) {
-    Session.set("filterByState", event.target.id);
-
-  // Scroll to Results
-  // target element id
-  var id = $("#search_heading");
-
-  // target element
-  var $id = $(id);
-  if ($id.length === 0) {
-      return;
-  }
-
-  // top position relative to the document
-  var pos = $(id).offset().top;
-
-  // animated top scrolling
-  $('body, html').animate({scrollTop: pos}, 1000);
-
-  // Set state select
-  document.getElementById("choose-state").value = event.target.id;
-
-  // Deactivate active state
-  // There should only be one active at a time
-  var elms = document.getElementsByClassName("active-state");
-  if(elms.length > 0)
-    elms[0].setAttribute("class", "land");
-
-  // Activate new state
-  event.target.setAttribute("class", "land active-state");
-  },
 
 });
