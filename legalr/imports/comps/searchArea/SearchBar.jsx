1cbafec5a720b4008007bd99dfd642fddbefa261
George Mgaloblishvili <gm2665@columbia.edu>
2018-02-15 13:59:10 -0500

    Started Advanced dropdown

--- legalr/imports/comps/searchArea/SearchBar.jsx
+++ legalr/imports/comps/searchArea/SearchBar.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { InputGroup, InputGroupAddon, InputGroupText, Input, InputGroupDropDown, InputGroupButtonDropdown } from 'reactstrap';
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Button } from 'reactstrap';
+import { Form, FormGroup, Label, FormText } from 'reactstrap';
 import { Container, Row, Col } from 'reactstrap';
 
 import './SearchBar.css';
@@ -40,12 +41,60 @@ export default class SearchBar extends Component {
       <InputGroupButtonDropdown className="input-group-btn" addonType="append" isOpen={this.state.splitButtonOpen} toggle={this.toggleSplit}>
         <DropdownToggle split outline className="button" />
         <DropdownMenu>
-          <DropdownItem header>Header</DropdownItem>
-          <DropdownItem disabled>Action</DropdownItem>
-          <DropdownItem>Another Action</DropdownItem>
-          <DropdownItem divider />
-          <DropdownItem>Another Action</DropdownItem>
+          <DropdownItem toggle={false}>
+            <FormGroup>
+              <Label for="exampleSelect">Select</Label>
+              <Input type="select" name="select" id="exampleSelect">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Input>
+            </FormGroup>
+          </DropdownItem>
+
+          <DropdownItem toggle={false}>
+            <FormGroup>
+              <Label for="exampleSelect">Select</Label>
+              <Input type="select" name="select" id="exampleSelect">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Input>
+            </FormGroup>
+          </DropdownItem>
+
+          <DropdownItem toggle={false}>
+            <FormGroup>
+              <Label for="exampleSelect">Select</Label>
+              <Input type="select" name="select" id="exampleSelect">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </Input>
+            </FormGroup>
+          </DropdownItem>
+
+          <Row className="text-center">
+
+          <DropdownItem toggle={false}><Col className="text-center"  md="12" sm="12" xs="12">
+            <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+               <DropdownToggle caret id='advanced-toggle' >
+                 Advanced
+              </DropdownToggle>
+             </Dropdown>
+          </Col>
+
+          </DropdownItem>
+
+          </Row>
         </DropdownMenu>
+
       </InputGroupButtonDropdown>
       <InputGroupAddon addonType="append" >
         <Button color="primary" className="btn-group text-center" id="searchButton">
