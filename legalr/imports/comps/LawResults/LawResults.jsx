eae96081aa916dbb83ead35c5ecb6ebc048f28b0
George Mgaloblishvili <gm2665@columbia.edu>
2018-03-10 15:48:55 -0500

    Results UI update
    Added data table

--- legalr/imports/comps/LawResults/LawResults.jsx
+++ legalr/imports/comps/LawResults/LawResults.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { Container, Row, Col } from 'reactstrap';
 
 import DataTable from './DataTable.jsx';
+import SearchToggle from './SearchToggle.jsx';
 
 
 import './LawResults.css';
@@ -13,11 +14,14 @@ export default class LawResults extends Component {
       <Container fluid id="ResultBackground">
         <Container>
         <Row>
-          <Col md="3" sm="3" xs="3" className="text-center" id="SearchContainer">
-            <li> a</li>
-            <li> b</li>
-            <li> c</li>
+          <Col md="3" sm="3" xs="3" className="text-center">
+            <Col md="12" sm="12" xs="12" className="text-center" id="SearchContainer">
+                  <SearchToggle />
+            </Col>
+            <Col md="12" sm="12" xs="12" className="text-center" id="RecommendationContainer">
+            </Col>
           </Col>
+
           <Col md="8" sm="8" xs="8" className="text-center" id="ResultsContainer">
               <DataTable />
           </Col>
